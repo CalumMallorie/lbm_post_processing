a9454fd200356ba6779ca6dfb7155c8f21b25534
CalumMallorie <calum.mallorie@ed.ac.uk>
2023-11-24 09:54:52 +0000

    remove ignored files

--- python_post_processing/data_classes/particle.py
+++
@@ -1,4 +0,0 @@
-class Particle:
-    """Class containing the particle time series data."""
-    def __init__(self) -> None:
-        pass
--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='python_post_processing',
-    version='0.1.0',
-    packages=find_packages(),
-    # Add any other necessary information here
-)
