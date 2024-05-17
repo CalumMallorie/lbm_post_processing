3b2a485cb9ddb150989e2b77965c124805506b08
CalumMallorie <calum.mallorie@ed.ac.uk>
2024-05-17 11:19:30 +0100

    Added readme and updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python_post_processing',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     # Add any other necessary information here
 )
