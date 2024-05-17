fcba16cac5cccc2983aee29c4127385ecc593c65
CalumMallorie <calum.mallorie@ed.ac.uk>
2024-05-17 11:45:01 +0100

    changed name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='python_post_processing',
+    name='lbm_post_processing',
     version='1.0.0',
     packages=find_packages(),
     # Add any other necessary information here
